861c13539bbef36e91dad89fd724174f51ef51df
John Wang <johncwang@gmail.com>
2025-01-04 20:34:37 -0800

    examples: `examples/log4hsell`: init

---
+++ examples/log4shell/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/grokify/gogrype"
+	"github.com/grokify/mogo/fmt/fmtutil"
+	"github.com/grokify/mogo/log/logutil"
+)
+
+func main() {
+	// uses Grype output file against:
+	// https://github.com/tothi/log4shell-vulnerable-app
+	f := "grype_log4shell-vulnerable-app.json"
+	g, err := gogrype.ReadFileGrypeOutputJSON(f)
+	logutil.FatalErr(err)
+	fmtutil.PrintJSON(g)
+	fmtutil.PrintJSON(g.GoVEXes())
+}
